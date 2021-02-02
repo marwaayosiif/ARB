d5d145eae4a2a3925839a4789eceb2817531e010
Marwa Yossef <marwaayosiif@yahoo.com>
2021-02-02 23:01:49 +0200

    Add project files.

---
+++ ARB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ARB.Startup))]
+namespace ARB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
