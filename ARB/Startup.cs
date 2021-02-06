d169afa0186df1827c63c43ec842463d63667986
Marwa Yossef <marwaayosiif@yahoo.com>
2021-02-06 15:00:16 +0200

    cros

--- ARB/Startup.cs
+++ ARB/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ARB.Startup))]
@@ -8,7 +10,20 @@ namespace ARB
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app);  
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors(builder =>
+                builder.WithOrigins("http://10.5.50.75:4200 ")
+                .AllowAnyMethod()
+                .AllowAnyHeader() 
+                );
+
         }
     }
 }
