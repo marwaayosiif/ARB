c138e1d6f2b6e446c1698e1153848625e38b1edc
Marwa Yossef <marwaayosiif@yahoo.com>
2021-02-09 15:38:32 +0200

    comboboxDone

--- ARB/Controllers/API/ComboBoxController.cs
+++ ARB/Controllers/API/ComboBoxController.cs
@@ -17,12 +17,96 @@ namespace ARB.Controllers.API
         {
             _context = new ApplicationDbContext();
         }
+
+        [Route("GetBiRads")]
+
+        public IHttpActionResult GetBiRads()
+        {
+            var test = _context.BiRads.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetRecommendation")]
+
+        public IHttpActionResult GetRecommendation()
+        {
+            var test = _context.Recommendations.ToList();
+
+            return Ok(test);
+        }
+        //Asymmetries
+
+        [Route("GetAsymmetries")]
+
+        public IHttpActionResult GetAsymmetries()
+        {
+            var test = _context.Asymmetries.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetMassMargin")]
+
+        public IHttpActionResult GetMassMargin()
+        {
+            var test = _context.MassMargin.ToList();
+
+            return Ok(test);
+        }
+
+
         // GET api/<ComboBox>
         [Route("GetMassDensities")]
 
         public IHttpActionResult GetMassDensities()
         {
             var test = _context.MassDensity.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetQuadrants")]
+
+        public IHttpActionResult GetQuadrants()
+        {
+            var test = _context.Quadrants.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetClockFaces")]
+
+        public IHttpActionResult GetClockFaces()
+        {
+            var test = _context.ClockFaces.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetClacificationTypicallyBenign")]
+
+        public IHttpActionResult GetClacificationTypicallyBenign()
+        {
+            var test = _context.ClacificationTypicallyBenign.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetClacificationSuspiciousMorphology")]
+
+        public IHttpActionResult GetClacificationSuspiciousMorphology()
+        {
+            var test = _context.ClacificationSuspiciousMorphology.ToList();
+
+            return Ok(test);
+        }
+
+        [Route("GetClacificationDistribution")]
+
+        public IHttpActionResult GetClacificationDistribution()
+        {
+            var test = _context.ClacificationDistribution.ToList();
 
             return Ok(test);
         }
