49fbb7372c9a395ff010c1743ce6da35232aef71
Marwa Yossef <marwaayosiif@yahoo.com>
2021-07-07 13:29:36 +0200

    Adding image

---
+++ ARB/Controllers/API/ImageController.cs
@@ -0,0 +1,48 @@
+﻿
+using ARB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ARB.Controllers.API
+{
+    public class ImageController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ImageController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        [Route("api/UploadImage")]
+        
+        public HttpResponseMessage UploadImage()
+        {
+            string imageName = null;
+            var httpRequest = HttpContext.Current.Request;
+            var postedFile = httpRequest.Files["Image"];
+            imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+            var filePath = HttpContext.Current.Server.MapPath("~/Images/" + imageName);
+            postedFile.SaveAs(filePath);
+
+            Image image = new Image()
+            {
+                ImageName = imageName
+            };
+            _context.Image.Add(image);
+            _context.SaveChanges();
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+
+        }
+
+    }
+}
